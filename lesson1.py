6fa67559ad0b71aed93c078228e27da37e2bacda
rik0079844-code5 <rik0079844@gmail.com>
2026-04-27 13:59:41 +0500

    Домашнее задание

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+my_height = 180
+print(my_height)
+my_name = "Кирилл"
+my_name = "Кирилл Станин"
+print(my_name)
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')      
+print_letter('т')
