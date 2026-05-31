c61e056f5c758dd3275beb2b5a9c66b7eb5aad9b
rik0079844-code <rik00798@gmail.com>
2026-05-31 21:29:57 +0500

    Добавил файлы для урока 4

--- 02_lesson/lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-is_year_leap = input('год ')
-
-year = int(is_year_leap)
-if year % 4 == 0:
-    result = True
-else:
-    result = False
-print(f'год {year}: {result}')
--- 03_lesson/lesson_3_task_1.py
+++
@@ -1,6 +0,0 @@
-from user import User
-
-my_user = User("Kirill", "Stanin")
-print(my_user.get_first_name())
-print(my_user.get_last_name())
-print(my_user.get_user_info())
